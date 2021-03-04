9281d5d792e9e601d8db5f9758c46da1d8e8ba68
Rajdeep Ghatore <r.ghatore@gmail.com>
2021-03-04 08:46:37 -0700

    practicing binding to directive properties

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -3,6 +3,7 @@ import {
   ElementRef,
   HostBinding,
   HostListener,
+  Input,
   OnInit,
   Renderer2
 } from '@angular/core';
@@ -11,23 +12,33 @@ import {
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') background: string = 'khaki';
+  @Input() defaultColour: string = 'khaki';
+  // @Input() highlightColour: string = 'salmon';
+  @Input('appBetterHighlight') highlightColour: string = 'salmon';
+  // @HostBinding('style.backgroundColor') background: string = 'khaki';
+  // @HostBinding('style.backgroundColor') background: string = this.defaultColour;
+  @HostBinding('style.backgroundColor') background: string;
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+
+  }
 
   ngOnInit(): void {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'khaki');
+    this.background = this.defaultColour;
   }
 
   @HostListener('mouseenter', ['$event'])
   onEnter(eventData: Event) {
     // console.log('eventData: ', eventData);
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'salmon');
-    this.background = 'salmon';
+    // this.background = 'salmon';
+    this.background = this.highlightColour;
   }
 
   @HostListener('mouseleave') onExit(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'khaki');
-    this.background = 'khaki';
+    // this.background = 'khaki';
+    this.background = this.defaultColour;
   }
 }
